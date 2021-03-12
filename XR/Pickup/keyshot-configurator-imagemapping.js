0742ebaff03414cb62a6cca9cc7d24d238e72344
LuxionJordan <69374962+LuxionJordan@users.noreply.github.com>
2021-03-12 12:04:44 -0800

    Seymour
    59

---
+++ XR/Pickup/keyshot-configurator-imagemapping.js
@@ -0,0 +1,43 @@
+imagemappingdata =
+[
+    {
+        "image": "Pickup.1.png",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "159+660",
+            "196+624"
+        ]
+    },
+    {
+        "image": "Pickup.2.png",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "159+606",
+            "196+628"
+        ]
+    },
+    {
+        "image": "Pickup.3.png",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "159+604",
+            "196+640"
+        ]
+    },
+    {
+        "image": "Pickup.4.png",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "159+660",
+            "196+628"
+        ]
+    }
+];
---
+++ XR/Pickup/keyshot-configurator-modeldata.js
@@ -0,0 +1,55 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "zebra.png",
+                    "multimaterials": [
+                        "159+660",
+                        "196+624"
+                    ],
+                    "name": "Zebra"
+                },
+                {
+                    "icon": "reverse%20zebra.png",
+                    "multimaterials": [
+                        "159+606",
+                        "196+628"
+                    ],
+                    "name": "Reverse Zebra"
+                },
+                {
+                    "icon": "white.png",
+                    "multimaterials": [
+                        "159+604",
+                        "196+640"
+                    ],
+                    "name": "White"
+                },
+                {
+                    "icon": "black.png",
+                    "multimaterials": [
+                        "159+660",
+                        "196+628"
+                    ],
+                    "name": "Black"
+                }
+            ],
+            "name": "Pickups"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "default.png",
+            "materialgroups": [
+                "Pickups"
+            ],
+            "modelset": 1004,
+            "name": "Default"
+        }
+    ],
+    "studios": []
+};
