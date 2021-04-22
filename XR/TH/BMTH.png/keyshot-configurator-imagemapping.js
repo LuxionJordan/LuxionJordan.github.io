3a87fa90e585f296be7a1f5f2e5d7d01c852e65c
LuxionJordan <69374962+LuxionJordan@users.noreply.github.com>
2021-04-22 12:26:25 -0700

    BMTH

---
+++ XR/TH/BMTH.png/keyshot-configurator-imagemapping.js
@@ -0,0 +1,75 @@
+imagemappingdata =
+[
+    {
+        "image": "BMTH.1.png",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "16+13",
+            "4+1",
+            "8+5"
+        ],
+        "studio": 4244
+    },
+    {
+        "image": "BMTH.2.png",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "16+15",
+            "4+3",
+            "8+6"
+        ],
+        "studio": 4244
+    },
+    {
+        "image": "BMTH.3.png",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "16+14",
+            "4+3",
+            "8+7"
+        ],
+        "studio": 4244
+    },
+    {
+        "image": "BMTH.4.png",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "16+13",
+            "4+1",
+            "8+5"
+        ],
+        "studio": 4245
+    },
+    {
+        "image": "BMTH.5.png",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "16+15",
+            "4+3",
+            "8+6"
+        ],
+        "studio": 4245
+    },
+    {
+        "image": "BMTH.6.png",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "16+14",
+            "4+3",
+            "8+7"
+        ],
+        "studio": 4245
+    }
+];
---
+++ XR/TH/BMTH.png/keyshot-configurator-modeldata.js
@@ -0,0 +1,67 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "icon": "standard.png",
+                    "multimaterials": [
+                        "4+1",
+                        "8+5",
+                        "16+13"
+                    ],
+                    "name": "Standard"
+                },
+                {
+                    "icon": "red%20%2f%20carbon%20%2f%20copper.png",
+                    "multimaterials": [
+                        "4+3",
+                        "8+6",
+                        "16+15"
+                    ],
+                    "name": "Red / Carbon / Copper"
+                },
+                {
+                    "checked": true,
+                    "icon": "blue%20%2f%20turquoise%20%2f%20red.png",
+                    "multimaterials": [
+                        "4+3",
+                        "8+7",
+                        "16+14"
+                    ],
+                    "name": "Blue / Turquoise / Red"
+                }
+            ],
+            "name": "Tap Materials"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "tap.png",
+            "materialgroups": [
+                "Tap Materials"
+            ],
+            "modelset": 1004,
+            "name": "Tap",
+            "productoptiongroups": [
+                {
+                    "name": "Component Group"
+                }
+            ]
+        }
+    ],
+    "studios": [
+        {
+            "checked": true,
+            "icon": "outside%20bar.png",
+            "id": 4244,
+            "name": "Outside Bar"
+        },
+        {
+            "icon": "inside%20bar.png",
+            "id": 4245,
+            "name": "Inside Bar"
+        }
+    ]
+};
